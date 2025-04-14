9f0dff5fa7cec28744b999194d509e88d34b862b
PRISCILLA PAYET <priscilla.ppayet@gmail.com>
2025-04-14 17:43:07 +0200

    Back to it �

--- backend/resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-
-
-<p><strong>Name:</strong> {{ $name }}</p>
-<p><strong>Email:</strong> {{ $email }}</p>
-<p><strong>Phone:</strong> {{ $phone }}</p>
-<p><strong>Message:</strong></p>
-<p>{{ $message }}</p>
